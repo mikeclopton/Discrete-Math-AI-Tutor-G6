9834ae7b016009cbd20b684c9a98c3fe024e51d3
Papapepper1 <grsv19@gmail.com>
2024-08-28 12:28:50 -0400

    Added Front-end Prototype

---
+++ DiscreteTutor Front-end/README.md
@@ -0,0 +1,2 @@
+# ED2 Prototype
+ ## Use "cd DiscreteTutor", "npm install", and "npm run dev" in the terminal to view
---
+++ DiscreteTutor Front-end/src/components/input.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Input = () => {
+  return (
+    <div>
+      <h2>Input your answer here</h2>
+      <input type="file" placeholder="Your answer..." />
+      <button type="submit">Submit</button>
+    </div>
+  );
+};
+
+export default Input;
