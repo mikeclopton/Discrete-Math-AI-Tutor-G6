209f1f1ad4006ec719e6238fc43965a0a49970ab
Papapepper1 <grsv19@gmail.com>
2024-09-05 18:25:13 -0400

    User can now select the type of input to be used

--- DiscreteTutor_Front-end/src/components/input.jsx
+++ DiscreteTutor_Front-end/src/components/input.jsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Input = () => {
+  const [submissionType, setSubmissionType] = useState('latex');
+
+  const handleTypeChange = (event) => {
+    setSubmissionType(event.target.value);
+  };
+
   return (
     <div>
       <h2>Input your answer here</h2>
-      <input type="file" placeholder="Your answer..." />
+      
+      <div>
+        <label>
+          <input
+            type="radio"
+            value="latex"
+            checked={submissionType === 'latex'}
+            onChange={handleTypeChange}
+          />
+          LaTeX
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="photo"
+            checked={submissionType === 'photo'}
+            onChange={handleTypeChange}
+          />
+          Photo
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="pen"
+            checked={submissionType === 'pen'}
+            onChange={handleTypeChange}
+          />
+          Pen
+        </label>
+      </div>
+
+      {submissionType === 'photo' && (
+        <input type="file" placeholder="Upload a photo..." />
+      )}
+
+      {submissionType === 'pen' && (
+        <textarea placeholder="Write your answer here..."></textarea>
+      )}
+
+      {submissionType === 'latex' && (
+        <textarea placeholder="Enter your LaTeX code here..."></textarea>
+      )}
+
       <button type="submit">Submit</button>
     </div>
   );
